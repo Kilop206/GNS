cac8fd4f68142cf85a2dd7882896574272b244b4
Kilop206 <guilhermextra118@gmail.com>
2026-03-09 07:09:45 -0300

    Fixed logic of time setting and how the event tick occours

--- core/src/SimulationClock.cpp
+++ core/src/SimulationClock.cpp
@@ -1,23 +1,22 @@
 #include "core/SimulationClock.h"
 
-namespace gns::core {
+namespace core {
 
-    SimulationClock::SimulationClock(double time_step)
-        : current_time_(0.0),
-        dt_(time_step)
+    SimulationClock::SimulationClock()
+        : current_time_(0.0)
     {
     }
 
-    void SimulationClock::tick() {
-        current_time_ += dt_;
+    void SimulationClock::tick(double delta_time_) {
+        current_time_ += delta_time_;
     }
 
     double SimulationClock::now() const {
         return current_time_;
     }
 
-    double SimulationClock::timeStep() const {
-        return dt_;
+    void SimulationClock::setTime(double new_current_time_) {
+        current_time_ = new_current_time_;
     }
 
 }
