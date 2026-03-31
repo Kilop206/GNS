a334cf2a2078560e480a0e5f1f79da9132aa5664
Kilop206 <guilhermextra118@gmail.com>
2026-03-31 07:29:33 -0300

    Started commenting code

--- core/src/engine/events/Event.cpp
+++ core/src/engine/events/Event.cpp
@@ -2,21 +2,27 @@
 #include "engine/core/SimulationEngine.hpp"
 
 namespace kns {
+
+	// Initialize the static member variable nextId_ to 0.
 	std::uint64_t kns::Event::nextId_ = 0;
 
+	// Default implementation of the execute method for the Event class.
 	void kns::Event::execute(kns::SimulationEngine& engine) {
 	}
 
+	// Constructor for the Event class that initializes the timestamp and assigns a unique ID to each event.
 	kns::Event::Event(std::uint64_t timestamp)
 		: timestamp_(timestamp)
 		, id_(nextId_++)
 	{
 	}
 
+	// Getter method for the timestamp of the event.
 	std::uint64_t kns::Event::getTimestamp() const noexcept {
 		return timestamp_;
 	}
 
+	// Getter method for the unique ID of the event.
 	std::uint64_t kns::Event::getId() const noexcept {
 		return id_;
 	}
