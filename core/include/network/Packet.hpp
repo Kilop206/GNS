a9de1ba016c987a0b66fc08c5cadb3f5bbcbd751
Guilherme-Doge <guilherme_doge1@estudante.sesisenai.org.br>
2026-04-01 11:58:21 -0300

    fix(PacketReceivedEvent.cpp): fixes logic bugs

--- core/include/network/Packet.hpp
+++ core/include/network/Packet.hpp
@@ -7,5 +7,17 @@ namespace kns {
         const int destination;
         int current_node;
         std::uint64_t creation_time;
+        std::size_t packet_size_bytes;
+
+        Packet(int source,
+               int destination,
+               int current_node,
+               std::uint64_t creation_time,
+               int packet_size_bytes)
+            : source(source),
+              destination(destination),
+              current_node(current_node),
+              creation_time(creation_time),
+              packet_size_bytes(packet_size_bytes) {}
     };
 }
