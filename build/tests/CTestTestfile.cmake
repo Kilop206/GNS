4e2f80232be4ed818f0bfc1df1790f65171efc65
Kilop206 <guilhermextra118@gmail.com>
2026-04-10 22:37:45 -0300

    feat(): added loss_prob flag on loading main.cpp

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/guilh/Desktop/KNS/tests
+# Build directory: C:/Users/guilh/Desktop/KNS/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("C:/Users/guilh/Desktop/KNS/build/tests/kns_tests-b12d07c_include.cmake")
