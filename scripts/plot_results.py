94c359ad74d202aac0ad4d2f634401aef3aa66f2
Kilop206 <guilhermextra118@gmail.com>
2026-04-11 17:24:53 -0300

    feat(automation): made python script that generates graphs using csv files

---
+++ scripts/plot_results.py
@@ -0,0 +1,45 @@
+import pandas
+import os
+import glob
+import matplotlib.pyplot as plt
+
+loss_probs = []
+loss_rates = []
+latencies = []
+
+def generateLossProbLossRate(loss_probs, loss_rates) :
+    plt.figure()
+    plt.plot(loss_probs, loss_rates)
+
+    plt.title("Loss Probabilites x Loss Rates")
+    plt.xlabel("Loss Probabilities")
+    plt.ylabel("Loss Rates")
+
+    plt.savefig("./results/loss_rate_vs_loss_prob.png")
+
+
+def generateLatencyLossProb(latencies, loss_probs) :
+    plt.figure()
+    plt.plot(loss_probs, latencies)
+
+    plt.title("Loss Probabilities x Latencies")
+    plt.xlabel("Loss Probabilities")
+    plt.ylabel("Latencies")
+
+    plt.savefig("./results/latency_vs_loss_prob.png")
+
+
+for file in glob.glob("./results/*.csv") :
+    df = pandas.read_csv(file)
+    loss_rates.append(df["Loss Rate"][0])
+    latencies.append(df["Average Latency"][0])
+
+    parts = file.split("_")
+    loss_prob = os.path.splitext(parts[3])[0]
+    loss_probs.append(float(loss_prob))
+
+sorted_data = sorted(zip(loss_probs, loss_rates, latencies))
+loss_probs, loss_rates, latencies = zip(*sorted_data)
+
+generateLossProbLossRate(loss_probs, loss_rates)
+generateLatencyLossProb(latencies, loss_probs)
