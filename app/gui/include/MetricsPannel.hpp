fcd4e8169a9cb7e7e77a3cdbad8abc57fd857a8f
Kilop206 <guilhermextra118@gmail.com>
2026-04-16 11:10:16 -0300

    feat: readd metrics panel

--- app/gui/include/MetricsPannel.hpp
+++ app/gui/include/MetricsPannel.hpp
@@ -1,4 +1,9 @@
-#pragma
+#pragma once
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
 
 #include "../../../core/include/engine/core/Stats.hpp"
 
--- app/gui/src/MetricsPannel.cpp
+++ app/gui/src/MetricsPannel.cpp
@@ -2,6 +2,18 @@
 
 namespace interface {
     void MetricsPannel::render(const kns::Stats& stats) {
-        
+
+        float avg_latency = 0.0f;
+        if (stats.packets_delivered > 0) {
+            avg_latency = stats.total_latency / stats.packets_delivered;
+        }
+
+        ImGui::Text(
+            "Packets sent: %d\nPackets Delivered: %d\nPackets Lost: %d\nAverage Latency: %.2f",
+            stats.packets_sent,
+            stats.packets_delivered,
+            stats.packets_lost,
+            avg_latency
+        );
     }
 }
