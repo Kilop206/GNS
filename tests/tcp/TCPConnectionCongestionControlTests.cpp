331413a5503ee57fe2ae4706a63862022075c034
Kilop206 <guilhermextra118@gmail.com>
2026-09-10 07:19:50 -0300

    chore: save before merge

--- tests/tcp/TCPConnectionCongestionControlTests.cpp
+++ tests/tcp/TCPConnectionCongestionControlTests.cpp
@@ -163,3 +163,147 @@ TEST_CASE(
         connection.getCongestionControl().getMss() == 1000
     );
 }
+
+TEST_CASE(
+    "TCPConnection forwards newly acknowledged bytes to congestion control",
+    "[tcp][congestion][connection]"
+)
+{
+    TCPConnection connection(
+        TCPState::ESTABLISHED,
+        1000,
+        2000,
+        0,
+        1,
+        CongestionControlType::RENO,
+        1000
+    );
+
+    TCPSegment segment;
+    segment.seq = 1000;
+    segment.payload.assign(1000, 0x41);
+
+    REQUIRE(
+        connection.queueSentSegment(
+            segment,
+            10.0
+        )
+    );
+
+    REQUIRE(
+        connection.getCongestionControl().getCwnd() == 1000
+    );
+
+    REQUIRE(
+        connection.receive_ack(
+            2000,
+            11.0
+        )
+    );
+
+    REQUIRE(
+        connection.getSendUnacknowledged() == 2000
+    );
+
+    REQUIRE(
+        connection.getCongestionControl().getCwnd() == 2000
+    );
+}
+
+TEST_CASE(
+    "TCPConnection congestion control enters congestion avoidance after ssthresh",
+    "[tcp][congestion][connection]"
+)
+{
+    TCPConnection connection(
+        TCPState::ESTABLISHED,
+        1000,
+        2000,
+        0,
+        1,
+        CongestionControlType::RENO,
+        1000
+    );
+
+    TCPSegment first;
+    first.seq = 1000;
+    first.payload.assign(1000, 0x41);
+
+    TCPSegment second;
+    second.seq = 2000;
+    second.payload.assign(1000, 0x42);
+
+    REQUIRE(
+        connection.queueSentSegment(
+            first,
+            10.0
+        )
+    );
+
+    REQUIRE(
+        connection.queueSentSegment(
+            second,
+            11.0
+        )
+    );
+
+    REQUIRE(
+        connection.receive_ack(
+            2000,
+            12.0
+        )
+    );
+
+    REQUIRE(
+        connection.getCongestionControl().getCwnd() == 2000
+    );
+
+    REQUIRE(
+        connection.receive_ack(
+            3000,
+            13.0
+        )
+    );
+
+    REQUIRE(
+        connection.getCongestionControl().getCwnd() == 2500
+    );
+}
+
+TEST_CASE(
+    "TCPConnection does not grow congestion window for invalid ACK",
+    "[tcp][congestion][connection]"
+)
+{
+    TCPConnection connection(
+        TCPState::ESTABLISHED,
+        1000,
+        2000,
+        0,
+        1,
+        CongestionControlType::RENO,
+        1000
+    );
+
+    TCPSegment segment;
+    segment.seq = 1000;
+    segment.payload.assign(1000, 0x41);
+
+    REQUIRE(
+        connection.queueSentSegment(
+            segment,
+            10.0
+        )
+    );
+
+    REQUIRE_FALSE(
+        connection.receive_ack(
+            2500,
+            11.0
+        )
+    );
+
+    REQUIRE(
+        connection.getCongestionControl().getCwnd() == 1000
+    );
+}
